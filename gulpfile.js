18c08fcff8776e2949412699abb34978884ea9e9
Doug Wollison <doug@wollison.net>
2021-04-07 13:53:16 -0400

    add index.html to test folder, test with watchAll

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-const { compileAll } = require( './index.js' )( {
+const { watchAll } = require( './index.js' )( {
 	subdirs: 'test',
+	syncBaseDir: 'test',
 } );
 
-exports.default = compileAll;
+exports.default = watchAll;
