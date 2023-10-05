3274a0b5a4e6e056ea0a8765d8443cf5aa316821
Doug Wollison <doug@wollison.net>
2023-10-05 12:52:41 -0400

    upgrade all packages, adjust setup
    - chalk not compatible, something something es modules; drop
    - logger not working, drop and just use console directly
    - sourcemaps and gitignore are now baked into src/dest
    - replace terser with rollup 3.x version
    - apparently need to fix base/cwd now

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ const path = require( 'path' );
 
 // Gulp + utilities
 const { src, dest, watch, series, parallel, lastRun } = require( 'gulp' );
-const logger = require( 'gulplog' );
-const chalk = require( 'chalk' );
 const through = require( 'through2' );
 const getDependencies = require( './lib/dependencies' )();
 
 // General Gulp Plugins
 const filter = require( 'gulp-custom-filter' );
-const sourcemaps = require( 'gulp-sourcemaps' );
 const rename = require( 'gulp-rename' );
 
 // Style Handling
@@ -19,6 +16,7 @@ const sass = require( 'gulp-dart-sass' );
 const postcss = require( 'gulp-postcss' );
 const postcssPresetEnv = require( 'postcss-preset-env' );
 const cssnano = require( 'cssnano' );
+const csso = require( 'postcss-csso' );
 
 // Linting Handling
 const eslint = require( 'gulp-eslint-new' );
@@ -30,8 +28,8 @@ const { babel } = require( '@rollup/plugin-babel' );
 const json = require( '@rollup/plugin-json' );
 const { nodeResolve } = require( '@rollup/plugin-node-resolve' );
 const commonjs = require( '@rollup/plugin-commonjs' );
-const { terser } = require( 'rollup-plugin-terser' );
 const replace = require( '@rollup/plugin-replace' );
+const terser = require( '@rollup/plugin-terser' );
 
 // =========================
 // ! Utilities
@@ -45,9 +43,9 @@ function sinceLastTask( task ) {
 }
 
 // Log the filename, formatted with a custom string
-function log( format, color = 'cyan' ) {
+function log( format ) {
 	return through.obj( function( file, encoding, callback ) {
-		logger.info( format, chalk[ color ]( file.relative ) );
+		console.info( format, file.relative );
 
 		// pass through
 		callback( null, file );
@@ -62,11 +60,6 @@ function mtime() {
 	} );
 }
 
-// Shortcut to gulp-custom-filter; gitingore
-function gitignore() {
-	return filter( filter.ignore( '.gitignore' ) );
-}
-
 // For gulp-custom-filter; exclude those with no changes (including dependencies)
 function checkDependencies( task ) {
 	const { since } = sinceLastTask( task );
@@ -79,7 +72,7 @@ function checkDependencies( task ) {
 
 		// File in question has changed, continue
 		if ( fs.statSync( file.path ).mtime.getTime() > since ) {
-			logger.info( 'Change detected to %s', chalk.blue( file.path.substr( file.base.length ) ) );
+			console.info( 'Change detected to %s', file.path.substr( file.base.length ) );
 			return true;
 		}
 
@@ -92,7 +85,7 @@ function checkDependencies( task ) {
 			const stats = fs.statSync( dep );
 
 			if ( stats.mtime.getTime() > since ) {
-				logger.info( 'Change detected to %s', chalk.blue( dep.substr( file.base.length ) ) );
+				console.info( 'Change detected to %s', dep.substr( file.base.length ) );
 				hasChanges = true;
 			}
 		} );
@@ -176,12 +169,12 @@ function defaultBoilerplate( config = {} ) {
 	} );
 
 	const { paths, watchOptions, syncStartPath, syncBaseDir = '' } = config;
-	let { postcssPlugins, postcssPresetEnvConfig, cssnanoConfig, rollupPlugins, syncWatchFiles } = config;
+	let { postcssPlugins, postcssPresetEnvConfig, cssoConfig, rollupPlugins, syncWatchFiles } = config;
 
 	if ( ! postcssPlugins ) {
 		postcssPlugins = [
 			postcssPresetEnv( postcssPresetEnvConfig ),
-			cssnano( cssnanoConfig ),
+			csso( cssoConfig ),
 		];
 	}
 
@@ -218,20 +211,23 @@ function defaultBoilerplate( config = {} ) {
 		];
 	}
 
+	const gulpSrc = {
+		dot: true,
+		cwdbase: true,
+	};
+
 	// =========================
 	// ! Style Handling
 	// =========================
 
+
 	function compileStyles() {
-		return src( paths.styles.input )
-			// Exclude those not part of the repo (e.g. other plugins/themes)
-			.pipe( gitignore() )
+		return src( paths.styles.input, { ...gulpSrc, sourcemaps: true } )
+			.pipe( log( 'checking %s' ) )
 			// Exclude those that haven't been modified (including dependencies)
 			.pipe( checkDependencies( compileStyles ) )
 			// Print the filename for reference
 			.pipe( log( 'Compiling %s' ) )
-			// With sourcemaps
-			.pipe( sourcemaps.init() )
 			// With Dart Sass
 			.pipe( sass() )
 			.on( 'error', sass.logError )
@@ -241,11 +237,10 @@ function defaultBoilerplate( config = {} ) {
 			.pipe( rename( output => {
 				output.dirname = output.dirname.replace( 'scss', 'css' );
 			} ) )
-			.pipe( log( 'Compiled %s', 'green' ) )
+			.pipe( log( 'Compiled %s' ) )
 			// Save sourcemaps to same folder
-			.pipe( sourcemaps.write( paths.styles.output ) )
 			.pipe( mtime() )
-			.pipe( dest( paths.styles.output ) );
+			.pipe( dest( paths.styles.output, { sourcemaps: '.' } ) );
 	}
 
 	// =========================
@@ -254,9 +249,7 @@ function defaultBoilerplate( config = {} ) {
 
 	function validateScripts() {
 		// Check all watchable scripts
-		return src( paths.scripts.lint, sinceLastTask( validateScripts ) )
-			// Exclude those not part of the repo (e.g. other plugins/themes)
-			.pipe( gitignore() )
+		return src( paths.scripts.lint, { ...gulpSrc, ...sinceLastTask( validateScripts ) } )
 			// Print the filename for reference
 			.pipe( log( 'Linting %s' ) )
 			// Lint and display issues
@@ -265,15 +258,11 @@ function defaultBoilerplate( config = {} ) {
 	}
 
 	function compileScripts() {
-		return src( paths.scripts.input )
-			// Exclude those not part of the repo (e.g. other plugins/themes)
-			.pipe( gitignore() )
+		return src( paths.scripts.input, { ...gulpSrc, sourcemaps: true } )
 			// Exclude those that haven't been modified (including dependencies)
 			.pipe( checkDependencies( compileScripts ) )
 			// Print the filename for reference
 			.pipe( log( 'Bundling %s' ) )
-			// With sourcemaps
-			.pipe( sourcemaps.init() )
 			// Transpile and Bundle
 			.pipe( rollupEach(
 				{
@@ -299,10 +288,8 @@ function defaultBoilerplate( config = {} ) {
 				output.basename = output.basename + '.min';
 			} ) )
 			.pipe( log( 'Bundled %s', 'green' ) )
-			// Save sourcemaps to same folder
-			.pipe( sourcemaps.write( paths.scripts.output ) )
 			.pipe( mtime() )
-			.pipe( dest( paths.scripts.output ) );
+			.pipe( dest( paths.scripts.output, { sourcemaps: '.' } ) );
 	}
 
 	// =========================
